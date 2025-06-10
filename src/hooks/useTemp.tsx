0f7c0e6797bc31071ea8c3fdb9ea104aa7abe7e4
Alendroide <alendroideyt@gmail.com>
2025-06-09 19:00:00 -0500

    useTemp hook and TempChart component created

---
+++ src/hooks/useTemp.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState, useRef } from "react";
+import mqtt from "mqtt";
+import { ChartData } from "chart.js";
+
+export default function useTemp() {
+
+  const [currentTemperature, setCurrentTemperature] = useState(0);
+  const [chartData, setChartData] = useState<ChartData<"line">>({
+    labels: [], // timestamps
+    datasets: [
+      {
+        label: "Temperatura (°C)",
+        data: [],
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+      },
+    ],
+  });
+
+  const dataRef = useRef<{ labels: string[]; data: number[] }>({
+    labels: [],
+    data: [],
+  });
+
+  useEffect(() => {
+    const client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt");
+
+    client.on("connect", () => {
+      console.log("✅ Conectado a MQTT");
+      client.subscribe("pepe/esp32/temperatura");
+    });
+
+    client.on("message", (topic, message) => {
+      try {
+        console.log(topic);
+        const payload = JSON.parse(message.toString());
+
+        const degrees = payload.degrees;
+        const createdAt = new Date().toISOString();
+
+        setCurrentTemperature(degrees);
+
+        dataRef.current.labels.push(createdAt);
+        dataRef.current.data.push(degrees);
+
+        if (dataRef.current.labels.length > 20) {
+          dataRef.current.labels.shift();
+          dataRef.current.data.shift();
+        }
+
+        setChartData({
+          labels: [...dataRef.current.labels],
+          datasets: [
+            {
+              label: "Temperatura (°C)",
+              data: [...dataRef.current.data],
+              fill: false,
+              borderColor: "rgb(75, 192, 192)",
+              tension: 0.1,
+            },
+          ],
+        });
+      } catch (err) {
+        console.error("❌ Error al procesar mensaje MQTT", err);
+      }
+    });
+
+    return () => {
+      client.end();
+    };
+  }, []);
+
+  return {chartData, currentTemperature};
+}
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,129 +1,20 @@
 import DefaultLayout from "@/layouts/DefaultLayout";
-import { useEffect, useState, useRef } from "react";
-import { io } from "socket.io-client";
-import {format} from 'date-fns';
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  TimeScale,
-  Tooltip,
-  Legend,
-  ChartData,
-  ChartOptions,
-} from "chart.js";
-import 'chartjs-adapter-date-fns';
-import { Line } from "react-chartjs-2";
 
-ChartJS.register(
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  TimeScale,
-  Tooltip,
-  Legend
-);
+import TempChart from "@/components/molecules/TempChart";
+import useTemp from "@/hooks/useTemp";
+
+
 
 export default function IndexPage() {
-  const [currentTemperature, setCurrentTemperature] = useState(0);
-  const [chartData, setChartData] = useState<ChartData<"line">>({
-    labels: [], // timestamps
-    datasets: [
-      {
-        label: "Temperatura (°C)",
-        data: [],
-        fill: false,
-        borderColor: "rgb(75, 192, 192)",
-        tension: 0.1,
-      },
-    ],
-  });
 
-  // Usamos ref para no perder los datos anteriores
-  const dataRef = useRef<{ labels: string[]; data: number[] }>({ labels: [], data: [] });
-
-  useEffect(() => {
-    const socket = io(import.meta.env.VITE_API_URL);
-
-    socket.on("new-temperature", (data : {degrees : number, createdAt : string}) => {
-      setCurrentTemperature(data.degrees);
-
-      // Agregar nueva entrada
-      dataRef.current.labels.push(data.createdAt);
-      dataRef.current.data.push(data.degrees);
-
-      // Limitar a los últimos 20 puntos (opcional)
-      if (dataRef.current.labels.length > 20) {
-        dataRef.current.labels.shift();
-        dataRef.current.data.shift();
-      }
-
-      setChartData({
-        labels: [...dataRef.current.labels],
-        datasets: [
-          {
-            label: "Temperatura (°C)",
-            data: [...dataRef.current.data],
-            fill: false,
-            borderColor: "rgb(75, 192, 192)",
-            tension: 0.1,
-          },
-        ],
-      });
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  const options: ChartOptions<"line"> = {
-    responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-      },
-    },
-    scales: {
-      x: {
-        type: 'time',
-        time: {
-          tooltipFormat: 'HH:mm:ss',
-          unit: 'second',
-          displayFormats: {
-            second: "ss"
-          }
-        },
-        ticks: {
-          callback: (value) => {
-            const date = new Date(Number(value));
-            return format(date, 'ss') + ' seg.';
-          }
-        },
-        title: {
-          display: true,
-          text: 'Hora',
-        },
-      },
-      y: {
-        min: 0,
-        title: {
-          display: true,
-          text: '°C',
-        },
-      },
-    },
-  };
+  const { currentTemperature, chartData } = useTemp();
 
   return (
     <DefaultLayout>
       <div className="flex h-full">
 
         <div className="w-7/12 h-96 bg-white p-3 rounded-3xl shadow-xl flex justify-center">
-          <Line data={chartData} options={options} />
+          <TempChart data={chartData} />
         </div>
 
         <div className="w-5/12 box-border p-6 ms-3">
